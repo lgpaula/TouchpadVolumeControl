88966023467b7e2e7914b4d80814aacf368d9249
lgpaula <l.gdepaula@hotmail.com>
2024-04-22 22:07:49 +0200

    Code cleanup, parsing the devices for easier access to touchpad

--- main.cpp
+++ main.cpp
@@ -8,10 +8,10 @@
 #include <numeric>
 #include <algorithm>
 
-struct libevdev* find_device_by_name(const std::string& requested_name) {
+struct libevdev* find_device_by_name(const std::string& requested_name, int event) {
     struct libevdev *dev = nullptr;
 
-    std::string path = "/dev/input/event9";
+    std::string path = "/dev/input/event" + std::to_string(event);
     int fd = open(path.c_str(), O_RDWR|O_CLOEXEC);
     if (fd == -1) return nullptr;
 
@@ -26,6 +26,32 @@ struct libevdev* find_device_by_name(const std::string& requested_name) {
     return nullptr;
 }
 
+struct libevdev* getDevice() {
+    struct libevdev *dev = nullptr;
+
+    for (int i = 0;; i++) {
+        std::string path = "/dev/input/event" + std::to_string(i);
+        int fd = open(path.c_str(), O_RDWR|O_CLOEXEC);
+
+        if (fd == -1) break;
+
+        if (libevdev_new_from_fd(fd, &dev) == 0) {
+            if (libevdev_get_phys(dev) == nullptr) continue;
+            std::string phys = libevdev_get_phys(dev);
+            std::string name = libevdev_get_name(dev);
+
+            libevdev_free(dev);
+            dev = nullptr;
+
+            if (name.find("Touchpad") != std::string::npos) return find_device_by_name(name, i);
+        }
+        close(fd);
+        if (i > 20) break;
+    }
+
+    return nullptr;
+}
+
 int get_y_average(const std::deque<int>& y_values) {
     for (auto y : y_values) {
         std::cout << y << " ";
@@ -33,7 +59,7 @@ int get_y_average(const std::deque<int>& y_values) {
     return std::accumulate(y_values.begin(), y_values.end(), 0) / 3;
 }
 
-//todo account for 4 finger touch
+// todo keep track of each individual touch and average each last value
 void process_events_touchpad(struct libevdev *dev) {
 
     struct input_event ev = {};
@@ -51,8 +77,6 @@ void process_events_touchpad(struct libevdev *dev) {
 
     while (status = libevdev_next_event(dev, flags, &ev), !is_error(status)) {
         if (!has_next_event(status)) continue;
-
-        // come up with an equation in which the faster the average increases, the more volume increases.
 
         if (ev.type == 1 && ev.code == 334) {
             std::cout << "Got triple touch" << std::endl;
@@ -86,71 +110,29 @@ void process_events_touchpad(struct libevdev *dev) {
             }
 
             auto diff = average_y - previous_average;
-            if (std::abs(diff) >= 10) {
-                std::cout << std::endl;
-                continue;
-            }
-            std::cout << " --> diff: " << diff;
-            volume -= diff / 2;
+            diff = std::clamp(diff, -2, 2);
+            std::cout << " --> " << (diff > 0 ? "up" : diff < 0 ? "down" : "eq");
+            volume -= diff;
             volume = std::clamp(volume, 0, 100);
             std::cout << " --> volume: " << volume << std::endl;
 
             previous_average = average_y;
-
-//            std::cout << "y pos: ";
-//            std::cout << ev.value << std::endl;
         }
-        BTN_LEFT;
-
-        //code 272 left click type1
-        //code 273 right click type1
-
-    }
-}
-
-void getDevices() {
-    struct libevdev *dev = nullptr;
-
-    for (int i = 0;; i++) {
-        std::string path = "/dev/input/event" + std::to_string(i);
-        int fd = open(path.c_str(), O_RDWR|O_CLOEXEC);
-        if (fd == -1) {
-            continue; // no more character devices
-        }
-        std::cout << i << ", " << fd << std::endl;
-        if (libevdev_new_from_fd(fd, &dev) == 0) {
-            if (libevdev_get_phys(dev) == nullptr) continue;
-            std::string phys = libevdev_get_phys(dev);
-            std::string name = libevdev_get_name(dev);
-
-            std::cout << path << std::endl;
-            std::cout << "- phys: " << phys << std::endl;
-            std::cout << "- name: " << name << std::endl;
-            std::cout << std::endl;
-
-            libevdev_free(dev);
-            dev = nullptr;
-        }
-        close(fd);
-        if (i > 20) break;
     }
 }
 
 int main() {
-//    getDevices();
-    struct libevdev *dev = find_device_by_name("SYNA2B46:00 06CB:CD5F Touchpad");
+    auto *dev = getDevice();
 
     if (dev == nullptr) {
         std::cerr << "Couldn't find device!" << std::endl;
         return -1;
     }
 
-    libevdev_grab(dev, LIBEVDEV_GRAB);
-
     process_events_touchpad(dev);
-
     libevdev_free(dev);
     return 0;
+    BTN_LEFT;
 }
 
 
@@ -189,7 +171,7 @@ Got input_event type=3 code=53 value=743
 Got input_event type=3 code=54 value=360
 Got input_event type=1 code=325 value=0
 Got input_event type=1 code=333 value=1         double tap
-Got input_event type=4 code=5 value=7200
+Got input_event type=4 code=5 value=7200        timestamp
 Got input_event type=0 code=0 value=0
 Got input_event type=4 code=5 value=14400
 Got input_event type=0 code=0 value=0
